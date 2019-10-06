bbc0a85cd32b5daf1b57d28c93d7a0b8467ecc6a
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2019-10-06 17:40:22 -0400

    handlebars update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
-var expressHandlebars = require("exress-handlears");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8080;
@@ -20,6 +21,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error){
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 app.listen(PORT, function(){
     console.log("Listening on port:" + PORT);
